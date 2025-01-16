b75a0094bfa59af01e483e4ef6cbc5426dc552ad
Dilnur Yuldashev <dilnuryuldashev@gmail.com>
2025-01-15 17:22:23 -0800

    Empty Memorize Project

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Dilnur Yuldashev on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
