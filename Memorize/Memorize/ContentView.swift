01650fb406b7d4e35675b434f4b4eff4a0a4f873
Dilnur Yuldashev <dilnuryuldashev@gmail.com>
2025-01-27 19:44:18 -0800

    Card Face up/down implemented

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp: Bool = false
     
     var body: some View {
         ZStack {
@@ -35,6 +35,9 @@ struct CardView: View {
             } else {
                 RoundedRectangle(cornerRadius: 12)
             }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
         }
     }
 }
