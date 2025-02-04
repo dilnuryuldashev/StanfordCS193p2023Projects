b690c2cefee84bdd750b85053da8b6cec0b556b8
Dilnur Yuldashev <dilnuryuldashev@gmail.com>
2025-02-03 17:38:48 -0800

    Assignment 3 New Project Added: Card Game Set

---
+++ SetCardGame/SetCardGame/SetCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetCardGameApp.swift
+//  SetCardGame
+//
+//  Created by Dilnur Yuldashev on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SetCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
