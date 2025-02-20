7479498001c4cf20a160f17b04e41ee51f01e08b
Dilnur Yuldashev <dilnuryuldashev@gmail.com>
2025-02-20 11:34:12 -0800

    Added .gitignore

---
+++ SetCardGame/SetCardGame/Views/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  SetCardGame
+//
+//  Created by Dilnur Yuldashev on 2/20/25.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
